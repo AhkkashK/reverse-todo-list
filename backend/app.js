90f4345b52fd79c7d991b74636394c68965d873c
Ahhkash Kandasamy <ahkkash.kandasamy@efrei.net>
2024-11-28 13:19:05 +0100

    yml and string

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoURI = process.env.MONGO_URL || "YOUR_MONGO_DB_CONNECTION_STRING";
+const mongoURI = process.env.MONGO_URL || "mongodb+srv://ahkkashkandasamy:uauU1Z7MtADAOgEL@cluster0.srmm0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
